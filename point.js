feeb27bcbc35f3be8d4b233ffde2cdbe543eaf0b
stefzouille <contact.lokcy@gmail.com>
2021-04-01 21:52:28 +0200

    add

--- point.js
+++ point.js
@@ -3,6 +3,7 @@
 // https://fr.wikipedia.org/wiki/Distance_entre_deux_points_sur_le_plan_cart%C3%A9sien
 // Pour calculer la racine carrée d'un nombre on utilise directement la méthode Math.sqrt:
 // Définit d'une classe Point
+
 // declaration de la class et de ses objet en parametre x,y
 class Point {
   constructor(x, y) {
@@ -22,11 +23,14 @@ class Point {
   //bon le calcule distance ramene a une formule    !(a+ b)² = a² + 2ab + b²          (a - b)² = a² - 2ab -b²
   
   distance(dt) {
+    //Pour calculer la racine carrée d'un nombre on utilise directement la méthode Math.sqrt: Math.sqrt(9) // retourne 3
+    //Pour mettre au carré une nombre on le multiplie par lui même ou sinon on utilise la méthode Math.pow:
+    //ATTENTION la Math.pow en 1er parametre c est le nb le 2eme parametre c est le nb de la puissance
       console.log(Math.sqrt((Math.pow((dt.x - this.x),2)) + (Math.pow((dt.y - this.y),2))))
-  }//        
+  }        
 }
 
 
-// Point = tete 
+//phrase pour export
 exports.Point = Point
 
