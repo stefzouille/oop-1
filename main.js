18c756d56bc1ef2ef3c6e82d58cc60f8258095bb
stefzouille <contact.lokcy@gmail.com>
2021-04-02 09:18:25 +0200

    add

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 
-//appel 
+//fait appel/import du fichier classinfo ou l export est placé
 const { Point } = require('./point.js')
 
 
-
+//declaration des distances dans ma class d'objets 
 const p1 = new Point(1, 2)
 
 const p2 = new Point(10, 22)
 
+//passe une comparaison de l arrivé p2 et du depart p1 a ma methode
 p1.distance(p2)
 
 p1.isEqual(p2)
-// p1.show()
-// p2.show()
+
 
 // console.log(Math.sqrt(9))
 // console.log(Math.pow(3,2))
